f33e04bdf6763f0fff5cd877f9bab35ae5851b78
jrmrys2 <jeromereyes.acc@gmail.com>
2026-06-13 09:10:22 +0800

    use has_secured_password instead of manual encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+    has_secure_password
+
     validates :email, presence: true, uniqueness: true,
                format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :username, presence: true, uniqueness: true
-    validates :encrypted_password, presence: true
+    validates :username,
+        presence: true,
+        uniqueness: true,
+        length: { in: 3..20 },
+        format: {
+            with: /\A[a-zA-Z0-9_-]+\z/,
+            message: "can only contain letters, numbers, underscore, and hyphen"
+        }
+    validates :password, presence: true, length: { minimum: 6 }
+    
     enum role: { super_admin: 0, admin: 1 }
 
-    before_save :encrypt_password, :downcase_email
+    before_save :downcase_email
 
     private
 
     def downcase_email
         self.email = email.downcase
-    end
-
-    def encrypt_password
-        self.encrypted_password = BCrypt::Password.create(encrypted_password) if encrypted_password_changed?
     end
 end
